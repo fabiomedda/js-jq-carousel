ecf5373cde4ad869528781e79f2203d431202149
fabiomedda <meddafabio@gmail.com>
2020-11-28 18:10:52 +0100

    aggiunta e semplificazione codice

--- booleancareers-lc-ex-slider-layout-2f071b60c3dd/main.js
+++ booleancareers-lc-ex-slider-layout-2f071b60c3dd/main.js
@@ -1,54 +1,45 @@
 $(function () {
-  
+
   var next = $(".next");
   var prev = $(".prev");
   next.click(function () {
-    $(".active").next().addClass("active");
-    $(".active").prev().removeClass("active");
-  })
+    if ($(".last").hasClass("active")) {
+      $(".last").removeClass("active");
+      $(".first").addClass("active");
+    } else {
+      $(".active").next().addClass("active");
+      $(".active").prev().removeClass("active");
+    }
+  });
 
   prev.click(function () {
-    $(".active").prev().addClass("active");
-    $(".active").next().removeClass("active");
-  })
-
-  $("i.first").addClass("1");
-  $("i.first").next().addClass("2");
-  $("i.last").prev().addClass("3");
-  $("i.last").addClass("4");
-
-  $("img.first").addClass("1");
-  $("img.first").next().addClass("2");
-  $("img.last").prev().addClass("3");
-  $("img.last").addClass("4");
-
-
-  $("i.1").click(function () {
-    $(".1").addClass("active");
-    $(".2").removeClass("active");
-    $(".3").removeClass("active");
-    $(".4").removeClass("active");
+    if ($(".first").hasClass("active")) {
+      $(".first").removeClass("active");
+      $(".last").addClass("active");
+    } else {
+      $(".active").prev().addClass("active");
+      $(".active").next().removeClass("active");
+    }
   });
 
-  $("i.2").click(function () {
-    $(".1").removeClass("active");
-    $(".2").addClass("active");
-    $(".3").removeClass("active");
-    $(".4").removeClass("active");
+  $("i.first").click(function () {
+    $(".images > img, .nav > i").removeClass("active");
+    $(".images > img.first, .nav > i.first").addClass("active");
   });
 
-  $("i.3").click(function () {
-    $(".1").removeClass("active");
-    $(".2").removeClass("active");
-    $(".3").addClass("active");
-    $(".4").removeClass("active");
+  $("i.first").next().click(function () {
+    $(".images > img, .nav > i").removeClass("active");
+    $(".images > img.first, .nav > i.first").next().addClass("active");
   });
 
-  $("i.4").click(function () {
-    $(".1").removeClass("active");
-    $(".2").removeClass("active");
-    $(".3").removeClass("active");
-    $(".4").addClass("active");
+  $("i.last").prev().click(function () {
+    $(".images > img, .nav > i").removeClass("active");
+    $(".images > img.last, .nav > i.last").prev().addClass("active");
+  });
+
+  $("i.last").click(function () {
+    $(".images > img, .nav > i").removeClass("active");
+    $(".images > img.last, .nav > i.last").addClass("active");
   });
 
 });
